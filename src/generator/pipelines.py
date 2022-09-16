1d7da3e70c8d7afded3750b14f38640be30b58fa
Don Kackman <dkackman@gmail.com>
2022-09-16 01:58:19 +0000

    logging

--- src/generator/pipelines.py
+++ src/generator/pipelines.py
@@ -5,16 +5,15 @@ import pickle
 
 
 def preload_pipelines(model_name, auth_token, pipeline_names = ["txt2img", "img2img", "imginpaint"]):
-    logging.debug(f"Using device# {torch.cuda.current_device()} - {torch.cuda.get_device_name(torch.cuda.current_device())}")
-
     # this will preload all the pipelines and serialize them to disk
-    # get_pipeline will then retrive from disk, this accomplishes two things:
+    # get_pipeline will then retrive from disk, accomplishing two things:
     # 1 - pay the startup cost to get the model form hugging face only 1 time per process
-    # 2 keep them out of RAM (main and GPU) until actually needed
+    # 2 - keep them out of RAM (main and GPU) until actually needed
     # on demand they get deserialized and pushed to the gpu
     #
     # TODO #8 model the GPU as a class; including what pipeline is loaded and if it has a workload or not
     if "txt2img" in pipeline_names:
+        logging.debug("Loading txt2img")
         pipeline = StableDiffusionPipeline.from_pretrained(
             model_name,
             revision="fp16",
@@ -25,6 +24,7 @@ def preload_pipelines(model_name, auth_token, pipeline_names = ["txt2img", "img2
         pickle.dump(pipeline, open("/tmp/txt2img.pipeline", "wb"))
 
     if "img2img" in pipeline_names:
+        logging.debug("Loading img2img")
         pipeline = StableDiffusionImg2ImgPipeline.from_pretrained(
             model_name,
             revision="fp16", 
@@ -35,6 +35,7 @@ def preload_pipelines(model_name, auth_token, pipeline_names = ["txt2img", "img2
         pickle.dump(pipeline, open("/tmp/img2img.pipeline", "wb"))
 
     if "imginpaint" in pipeline_names:
+        logging.debug("Loading imginpaint")
         pipeline = StableDiffusionInpaintPipeline.from_pretrained(
             model_name,
             revision="fp16", 
@@ -46,6 +47,8 @@ def preload_pipelines(model_name, auth_token, pipeline_names = ["txt2img", "img2
 
 
 def get_pipeline(pipeline_name):
+    logging.debug(f"Deserializeg {pipeline_name} to {torch.cuda.current_device()} - {torch.cuda.get_device_name(torch.cuda.current_device())}") 
+
     with torch.no_grad():
         torch.cuda.empty_cache()
 
