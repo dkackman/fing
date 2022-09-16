67cf34eacd2622bbf218343108faecec484a3a9e
Don Kackman <dkackman@gmail.com>
2022-09-16 02:31:57 +0000

    more descriptive file name

--- src/generator/pipelines.py
+++ src/generator/pipelines.py
@@ -3,47 +3,51 @@ import logging
 from diffusers import StableDiffusionPipeline, StableDiffusionImg2ImgPipeline, StableDiffusionInpaintPipeline
 import pickle
 
+revision = "fp16"
+torch_dtype=torch.float16
 
-def preload_pipelines(model_name, auth_token, pipeline_names = ["txt2img", "img2img", "imginpaint"]):
+def preload_pipelines(model, auth_token, pipeline_names = ["txt2img", "img2img", "imginpaint"]):
     # this will preload all the pipelines and serialize them to disk
     # get_pipeline will then retrive from disk, accomplishing two things:
     # 1 - pay the startup cost to get the model form hugging face only 1 time per process
     # 2 - keep them out of RAM (main and GPU) until actually needed
     # on demand they get deserialized and pushed to the gpu
-    #
-    # TODO #8 model the GPU as a class; including what pipeline is loaded and if it has a workload or not
+    
+    global model_name #TODO - make this a class
+    model_name = model.replace("/", ".")
+
     if "txt2img" in pipeline_names:
         logging.debug("Loading txt2img")
         pipeline = StableDiffusionPipeline.from_pretrained(
-            model_name,
-            revision="fp16",
-            torch_dtype=torch.float16,
+            model,
+            revision=revision,
+            torch_dtype=torch_dtype,
             use_auth_token=auth_token,
         )
 
-        pickle.dump(pipeline, open("/tmp/txt2img.pipeline", "wb"))
+        pickle.dump(pipeline, open(f"/tmp/{model_name}.{revision}.txt2img.pipeline", "wb"))
 
     if "img2img" in pipeline_names:
         logging.debug("Loading img2img")
         pipeline = StableDiffusionImg2ImgPipeline.from_pretrained(
-            model_name,
-            revision="fp16", 
-            torch_dtype=torch.float16,
+            model,
+            revision=revision, 
+            torch_dtype=torch_dtype,
             use_auth_token=auth_token,
         )
 
-        pickle.dump(pipeline, open("/tmp/img2img.pipeline", "wb"))
+        pickle.dump(pipeline, open(f"/tmp/{model_name}.{revision}.img2img.pipeline", "wb"))
 
     if "imginpaint" in pipeline_names:
         logging.debug("Loading imginpaint")
         pipeline = StableDiffusionInpaintPipeline.from_pretrained(
-            model_name,
-            revision="fp16", 
-            torch_dtype=torch.float16,
+            model,
+            revision=revision, 
+            torch_dtype=torch_dtype,
             use_auth_token=auth_token,
         )
 
-        pickle.dump(pipeline, open("/tmp/imginpaint.pipeline", "wb"))
+        pickle.dump(pipeline, open(f"/tmp/{model_name}.{revision}.imginpaint.pipeline", "wb"))
 
 
 def get_pipeline(pipeline_name):
@@ -52,5 +56,5 @@ def get_pipeline(pipeline_name):
     with torch.no_grad():
         torch.cuda.empty_cache()
 
-    pipe = pickle.load(open(f"/tmp/{pipeline_name}.pipeline", "rb"))
+    pipe = pickle.load(open(f"/tmp/{model_name}.{revision}.{pipeline_name}.pipeline", "rb"))
     return pipe.to("cuda")
