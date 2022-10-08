678ee5f6ce86c051d8aef717b8ac0fc8e994ca2f
Don Kackman <dkackman@gmail.com>
2022-10-08 04:24:29 +0000

    lint

--- src/generator/diffusion/device_pool.py
+++ src/generator/diffusion/device_pool.py
@@ -23,6 +23,6 @@ def remove_device_from_pool() -> Device:
         if len(available) > 0:
             return available.pop(0)
 
-        raise (Exception("busy"))
+        raise Exception("busy")
     finally:
         mutex.release()
